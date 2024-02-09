f7152a05e0a189884446467b9a16e3b602eb24e6
permanai <iswahyudipermana@gmail.com>
2024-02-09 08:23:28 +0700

    comitper

--- resources/views/log.blade.php
+++ resources/views/log.blade.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <!-- Tambahkan link CSS Bootstrap atau CSS lainnya di sini jika diperlukan -->
+    
 </head>
 <body>
 
